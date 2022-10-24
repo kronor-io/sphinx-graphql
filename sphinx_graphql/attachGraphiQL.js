01335d9e20829c513b7633a24ad3ab2b7273526d
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2022-10-24 16:07:15 +0200

    add support for a data loading callback

--- sphinx_graphql/attachGraphiQL.js
+++ sphinx_graphql/attachGraphiQL.js
@@ -1,5 +1,5 @@
 // graphql query fetcher
-const graphQLFetcher = function (target, originalHeaders, query, response, variables, endpoint) {
+const graphQLFetcher = function (target, endpoint) {
     return function (graphQLParams, options) {
         const headers = options.headers || {};
         const params = {
@@ -18,14 +18,19 @@ const graphQLFetcher = function (target, originalHeaders, query, response, varia
             })
             .then(function (responseBody) {
                 try {
-                    return JSON.parse(responseBody);
+                    const parsed = JSON.parse(responseBody);
+
+                    if (window.onGraphiQLResponse !== undefined) {
+                        window.onGraphiQLResponse(target, parsed);
+                    }
+
+                    return parsed
                 } catch (error) {
+                    console.error(error)
                     return responseBody;
                 }
             }).catch(function (error) {
-                // Connection failed, remount read-only
-                ReactDOM.unmountComponentAtNode(target);
-                renderGraphiQL(target, originalHeaders, query, variables, response);
+                console.error(error);
                 return originalResponse;
             })
     }
@@ -35,32 +40,71 @@ const graphQLFetcher = function (target, originalHeaders, query, response, varia
 const attachGraphiQL = function (target) {
     const query = target.getElementsByClassName("query")[0].innerHTML.trim();
     const response = target.getElementsByClassName("response")[0].innerHTML.trim();
-    const variables = target.getElementsByClassName("variables")[0].innerHTML.trim();
     const headers = target.getElementsByClassName("headers")[0].innerHTML.trim();
     const endpoint = target.getElementsByClassName("endpoint")[0].innerHTML.trim();
-    window.sessionStorage.clear();
-    renderGraphiQL(target, headers, query, response, variables, endpoint);
-}
 
-const renderGraphiQL = function (target, headers, query, response, variables, endpoint) {
     if (endpoint == undefined) {
         target.classList.add("graphiql-ro");
     }
-    const graphiQLElement = React.createElement(GraphiQL, {
-        fetcher: graphQLFetcher(target, headers, query, response, variables, endpoint),
+
+    const variables = replaceVariables(target.getElementsByClassName("variables")[0].innerHTML.trim());
+    const fetcher = graphQLFetcher(target, endpoint);
+    const props = {
+        fetcher,
         schema: (endpoint == undefined) ? null : undefined,
         query: query,
         response: response,
         variables: variables,
         headers: headers,
-        storage: window.sessionStorage,
+        storage: storageMock(),
         defaultSecondaryEditorOpen: variables.length > 0,
         readOnly: (endpoint == undefined)
-    });
-    ReactDOM.render(graphiQLElement, target);
+    };
+
+    const root = ReactDOM.createRoot(target)
+    const graphiQLElement = renderGraphiQL(root, props);
+    window.attachedEditors.push({ graphiQLElement, root, target });
+}
+
+const renderGraphiQL = function (root, props) {
+    const graphiQLElement = React.createElement(GraphiQL, props);
+    root.render(graphiQLElement)
+    return graphiQLElement;
 };
 
+function replaceVariables(variables) {
+    if (variables == undefined) {
+        return variables;
+    }
+    const today = new Date()
+    const tomorrow = new Date(today)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+
+    variables = variables.replace(/now\(\) \+ 1 day/, tomorrow.toJSON());
+    variables = variables.replace(/\[randomnumber\]/, (Math.random() * 10e12).toFixed(0));
+    return variables;
+}
+
 window.addEventListener("load", function () {
+    window.attachedEditors = [];
     const editors = Array.from(document.getElementsByClassName("sphinx-graphiql"));
     editors.forEach(attachGraphiQL);
 });
+
+function storageMock() {
+    return {
+        setItem: function (key, value) {
+        },
+        getItem: function (key) {
+            return null;
+        },
+        removeItem: function (key) {
+        },
+        get length() {
+            return 0;
+        },
+        key: function (i) {
+            return null;
+        }
+    };
+}
